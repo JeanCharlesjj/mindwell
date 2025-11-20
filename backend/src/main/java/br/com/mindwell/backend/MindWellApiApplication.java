ef55d2415d7b4b5436fa1379814ecd31ff5a811a
Jean Charles <juniorcharles.zip@gmail.com>
2025-11-20 09:45:31 -0300

    chore: Initial commit - Estrutura Monorepo (Backend + Frontend)

---
+++ backend/src/main/java/br/com/mindwell/backend/MindWellApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.mindwell.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindWellApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindWellApiApplication.class, args);
+	}
+
+}
